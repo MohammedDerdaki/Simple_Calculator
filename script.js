d5e097625e077e505392cae2341c8124a9187e05
Mohammed Derdaki <mohammed.derdaki@gmail.com>
2024-07-11 21:38:59 +0100

    New Update

---
+++ script.js
@@ -0,0 +1,8 @@
+// Declare Variables
+let reset = document.querySelector("#reset");
+let monitor = document.querySelector("#monitor");
+
+// Actions & Functions
+function reset(){
+    monitor.innerHTML = 0;
+}
