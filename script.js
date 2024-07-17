ed362082ff24158f06d27d54a50dcb65d8148fb6
Mohammed Derdaki <mohammed.derdaki@gmail.com>
2024-07-17 20:40:42 +0100

    check

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // Declare variable 
 let result = document.querySelector("#calculResult");
 let equal = document.querySelector("#equal");
+let btn = document.querySelectorAll();
+
+function clickbtn(this){
+    for(let i = 0; i<btn.length; i++){
+        if (this.value == btn[i].value){
+            console.log(this.value);
+        }
+    }
+}
